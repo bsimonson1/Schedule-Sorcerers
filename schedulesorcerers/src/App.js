25ccbdbe7c743aa58d7b993f617b0baf7ae4c7af
eileen-an1 <anyilin2002@gmail.com>
2024-03-01 13:40:47 -0500

    slight changes to login page

--- schedulesorcerers/src/App.js
+++ schedulesorcerers/src/App.js
@@ -4,19 +4,24 @@ import './App.css'; // Import the CSS file for styling
 const LoginPage = () => {
   return (
     <div className="login-page-container">
-      <div className="login-form-container">
-        <h2>Login</h2>
-        <form>
-          <div className="form-group">
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" />
-          </div>
-          <button type="submit">Login</button>
-        </form>
+      <div className="left-side-bar">
+        <div className="login-form-container">
+          <h2>Login</h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+      <div className="right-side-bar">
+        <h2>Crystal Chrono</h2>
       </div>
     </div>
   );
