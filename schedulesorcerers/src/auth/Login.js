69908a59740953ac4dc7f1168e4a3253eb124220
eileen-an1 <anyilin2002@gmail.com>
2024-03-06 16:55:23 -0500

    reorganized files, added router

---
+++ schedulesorcerers/src/auth/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Login.css'; // Import the CSS file for styling
+
+const LoginPage = () => {
+  return (
+    <div className="login-page-container">
+      <div className="right-side-bar">
+        <h2>Crystal Chrono</h2>
+        <div className="small-line-white"/>
+        <p1>Don't have an account? Sign up today!</p1>
+        <button>Sign Up</button>
+      </div>
+
+      <div className="left-side-bar">
+        <h2>Welcome Back!</h2>
+        <div className="small-line-purple"/>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Email:</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ schedulesorcerers/src/home/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+    return (
+        <div>
+          <p>calendar here</p>
+        </div>
+      );
+};
+
+export default HomePage
