f1531b58675e54cd45cf685569297d4847f3a027
eileen-an1 <anyilin2002@gmail.com>
2024-03-11 00:24:49 -0400

    error message for empty email or pw

---
+++ schedulesorcerers/src/home/ExampleEvents.js
@@ -0,0 +1,30 @@
+const now = new Date()
+
+export default [
+  /* {
+    id: 0,
+    title: 'All Day Event very long title',
+    allDay: true,
+    start: new Date(2015, 3, 0),
+    end: new Date(2015, 3, 1),
+  }, */
+  {
+    id: 1,
+    title: 'ex 1',
+    start: new Date(2024, 2, 7),
+    end: new Date(2024, 2, 10),
+  },
+
+  {
+    id: 2,
+    title: 'ex 2',
+    start: new Date(2024, 2, 13, 0, 0, 0),
+    end: new Date(2024, 2, 20, 0, 0, 0),
+  },
+  {
+    id: 3,
+    title: 'ex 3',
+    start: new Date(2024, 2, 13, 0, 0, 0),
+    end: new Date(2024, 2, 13, 0, 0, 0),
+  },
+]
--- schedulesorcerers/src/home/HomePage.js
+++ schedulesorcerers/src/home/HomePage.js
@@ -1,19 +1,35 @@
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
+//import Calendar from 'react-calendar';
 import './HomePage.css';
-import 'react-calendar/dist/Calendar.css';
+//import './Calendar.css';
+
+import Calendar from './Calendar';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { Views } from 'react-big-calendar';
+import ExampleEvents from './ExampleEvents';
 
 const HomePage = () => {
-    const [date, setDate] = useState(new Date());
+    /*const [date, setDate] = useState(new Date());
 
     const onChange = (newDate) => {
         setDate(newDate);
     };
+    */
 
     return (
-        <div>
-            <Calendar onChange={onChange} value={date} />
-            <p>Selected date: {date.toDateString()}</p>
+        <div className="homepage-container">
+            <div className="top-bar">put menu, exp, etc. here</div>
+            {/*<Calendar onChange={onChange} value={date} />*/}
+            {/*<p>Selected date: {date.toDateString()}</p>*/}
+            <div className='calendar-container'>
+                <Calendar
+                //i'm limiting the view to only month to make it easier for now
+                views={[Views.MONTH]}
+                defaultView={[Views.MONTH]}
+                selectable={true}
+                events={ExampleEvents}
+                />
+            </div>
         </div>
     );
 };
